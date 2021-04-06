68d748dbdffff51295d1bd4a2421c65701173e23
rouanthompson <rouan.thompson@gmail.com>
2021-04-05 21:30:31 -0400

    prints floatResult and result

--- src/com/FirstJavaSteps/Main.java
+++ src/com/FirstJavaSteps/Main.java
@@ -3,6 +3,10 @@ package com.FirstJavaSteps;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int result = 8 + 2;
+	    double floatResult = (double)8 + 2;
+	    //
+        System.out.println(result);
+        System.out.println(floatResult);
     }
 }
