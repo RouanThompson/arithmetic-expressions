d72a274a3ef523085d1703c06f7d0eb62c62a71a
rouanthompson <rouan.thompson@gmail.com>
2021-04-06 22:47:09 -0400

    Changed floatResult to doubleResult to reflect the cast

--- src/com/FirstJavaSteps/Main.java
+++ src/com/FirstJavaSteps/Main.java
@@ -4,9 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	    int result = 8 + 2;
-	    double floatResult = (double)8 + 2;
-	    //
+	    double doubleResult = (double)8 + 2;
+	    // to print the result of a double or float at least 1 operand must be casted to that type
+        // 8 is prefixed with (double) to denote that it is being casted
+
         System.out.println(result);
-        System.out.println(floatResult);
+        System.out.println(doubleResult);
     }
 }
